95dcfe353ed9df30a2e17892cdd09866546063a0
Yahel Nachum <yahelnachum@Yahels-MacBook-Pro.local>
2022-05-19 16:33:18 -0400

    remove gist code

--- src/features/FileIOAdapter.js
+++ src/features/FileIOAdapter.js
@@ -142,30 +142,5 @@ export class FileIOAdapter {
       console.log("AWS DISABLED");
       console.log(fileName);
     }
-    // const octokit = new Octokit({
-    //   userAgent: "thesis_answers/v1.0",
-    //   // eslint-disable-next-line no-undef
-    //   auth: atob(process.env.REACT_APP_AUTH_TOKEN),
-    // });
-    // // eslint-disable-next-line no-undef
-    // const gistAnswerId = process.env.REACT_APP_GIST_ANSWER_ID;
-    // const url = `PATCH /gists/${gistAnswerId}`;
-    // const now = DateTime.utc().toFormat("yyyy-MM-dd-H-mm-ss-SSS-ZZZZ");
-    // const files = {};
-    // answersCSV = `${answersCSV}`;
-    // files[`answers-subject-${state.questions.participant_id}-${now}.csv`] = {
-    //   content: answersCSV,
-    // };
-    // const description = `Answer results for participant ${state.questions.participant_id} at ${now}`;
-    // console.log("submitting answers for " + description);
-    // const payloadObj = {
-    //   gist_id: gistAnswerId,
-    //   description: description,
-    //   files: files,
-    // };
-    // const response = await octokit.request(url, payloadObj);
-
-    // const status = response.status;
-    // console.log("answers submitted with status of " + status);
   };
 }
