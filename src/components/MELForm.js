c7185b0b997b13d9b96a5f7dcc7b21702bea9e10
Peter Cordone <pcordone@gmail.com>
2022-06-30 02:32:28 -0400

    Changed to sx styling.
    Also upgraded npm packages.

--- src/components/MELForm.js
+++ src/components/MELForm.js
@@ -1,10 +1,8 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DateTime } from "luxon";
 import {
-  createTheme,
   Grid,
   Button,
   FormLabel,
@@ -13,9 +11,9 @@ import {
   FormHelperText,
   Radio,
   RadioGroup,
-  Typography,
+  Box,
+  ThemeProvider,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 
 import { ChoiceType } from "../features/ChoiceType";
 import { StatusType } from "../features/StatusType";
@@ -29,23 +27,20 @@ import { format } from "d3";
 import { dateToState } from "../features/ConversionUtil";
 import { styles, theme } from "./ScreenHelper";
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-  formLabel: {
-    fontSize: 32,
-    color: "black",
-  },
-  formControlLabel: {
-    fontSize: 24,
-    color: "black",
-  },
-}));
+const formControl = {
+  margin: theme.spacing(1),
+  minWidth: 120,
+};
+
+const formLabel = {
+  fontSize: 32,
+  color: "black",
+};
+
+const formControlLabel = {
+  fontSize: 32,
+  color: "black",
+};
 
 export function MELForm() {
   const dispatch = useDispatch();
@@ -59,8 +54,6 @@ export function MELForm() {
   useEffect(() => {
     dispatch(dispatch(setQuestionShownTimestamp(dateToState(DateTime.utc()))));
   }, []);
-
-  const classes = useStyles();
 
   const todayText = (sooner_time) =>
     sooner_time === 0 ? "today" : `in ${sooner_time} months`;
@@ -78,88 +71,92 @@ export function MELForm() {
   }
 
   const result = (
-    <div>
-      <Grid container style={styles.root} justifyContent="center">
-        <Grid item xs={12}>
-          <form onSubmit={(event) => {}}>
-            <FormControl
-              className={classes.formControl}
-              required={false}
-              error={error}
-            >
-              <FormLabel className={classes.formLabel} id="question-text">
-                {questionText()}
-              </FormLabel>
-              <FormHelperText>{helperText}</FormHelperText>
-              <RadioGroup
-                row
-                aria-labelledby={q.textShort + "-row-radio-buttons-group-label"}
-                name={"question-radio-buttons-group"}
-                onChange={(event) => {
-                  setChoice(event.target.value);
-                  setHelperText("");
-                  setError(false);
-                }}
-                value={choice}
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Grid container style={styles.root} justifyContent="center">
+          <Grid item xs={12}>
+            <form>
+              <FormControl
+                sx={{ ...formControl }}
+                required={false}
+                error={error}
               >
-                <FormControlLabel
-                  className={classes.formControlLabel}
-                  key={ChoiceType.earlier}
-                  value={ChoiceType.earlier}
-                  checked={choice === ChoiceType.earlier}
-                  control={<Radio />}
-                  label={question1stPartText()}
-                />
-                <FormControlLabel
-                  className={classes.formControlLabel}
-                  key={ChoiceType.later}
-                  value={ChoiceType.later}
-                  checked={choice === ChoiceType.later}
-                  control={<Radio />}
-                  label={question2ndPartText()}
-                />
-              </RadioGroup>
-            </FormControl>
-          </form>
-        </Grid>
-        <Grid item xs={12} style={{ margin: 0 }}>
-          <Button
-            variant="contained"
-            color="secondary"
-            disableRipple
-            disableFocusRipple
-            style={styles.button}
-            onClick={() => {
-              if (
-                choice !== ChoiceType.earlier &&
-                choice !== ChoiceType.later
-              ) {
-                setError(true);
-                setHelperText("You must choose one of the options below.");
-              } else {
-                setError(false);
-                setHelperText("");
-                setTimeout(() => {
-                  dispatch(
-                    answer({
-                      choice: choice,
-                      choiceTimestamp: dateToState(DateTime.utc()),
-                    })
-                  );
-                  setChoice(null);
-                  if (status === StatusType.Questionaire) {
-                    navigate("/questionaire");
+                <FormLabel sx={{ ...formLabel }} id="question-text">
+                  {questionText()}
+                </FormLabel>
+                <FormHelperText>{helperText}</FormHelperText>
+                <RadioGroup
+                  row
+                  aria-labelledby={
+                    q.textShort + "-row-radio-buttons-group-label"
                   }
-                }, 400);
-              }
-            }}
-          >
-            {" "}
-            Next{" "}
-          </Button>
+                  name={"question-radio-buttons-group"}
+                  onChange={(event) => {
+                    setChoice(event.target.value);
+                    setHelperText("");
+                    setError(false);
+                  }}
+                  value={choice}
+                >
+                  <FormControlLabel
+                    sx={{ ...formControlLabel }}
+                    key={ChoiceType.earlier}
+                    value={ChoiceType.earlier}
+                    checked={choice === ChoiceType.earlier}
+                    control={<Radio />}
+                    label={question1stPartText()}
+                  />
+                  <FormControlLabel
+                    sx={{ ...formControlLabel }}
+                    key={ChoiceType.later}
+                    value={ChoiceType.later}
+                    checked={choice === ChoiceType.later}
+                    control={<Radio />}
+                    label={question2ndPartText()}
+                  />
+                </RadioGroup>
+              </FormControl>
+            </form>
+          </Grid>
+          <Grid item xs={12} style={{ margin: 0 }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              disableRipple
+              disableFocusRipple
+              style={styles.button}
+              onClick={() => {
+                if (
+                  choice !== ChoiceType.earlier &&
+                  choice !== ChoiceType.later
+                ) {
+                  setError(true);
+                  setHelperText("You must choose one of the options below.");
+                } else {
+                  setError(false);
+                  setHelperText("");
+                  setTimeout(() => {
+                    dispatch(
+                      answer({
+                        choice: choice,
+                        choiceTimestamp: dateToState(DateTime.utc()),
+                      })
+                    );
+                    setChoice(null);
+                    if (status === StatusType.Questionaire) {
+                      navigate("/questionaire");
+                    }
+                  }, 400);
+                }
+              }}
+            >
+              {" "}
+              Next{" "}
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </Box>
+    </ThemeProvider>
   );
 
   return result;
