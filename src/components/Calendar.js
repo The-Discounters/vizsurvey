abf6c10640ebc855b8668a9ec9b5a0c1e4a6b45b
Yahel Nachum <yahelnachum@Yahels-MacBook-Pro.local>
2023-01-07 06:47:52 -0500

    calendar completely showing up

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -11,11 +11,13 @@ import {
   answer,
 } from "../features/questionSlice";
 import { useD3 } from "../hooks/useD3";
+import { select } from "d3";
 import { AmountType } from "../features/AmountType";
 import { StatusType } from "../features/StatusType";
 import { InteractionType } from "../features/InteractionType";
-import { drawCalendar } from "./CalendarHelper";
-import { dateToState, stateToDate } from "../features/ConversionUtil";
+//import { drawCalendar } from "./CalendarHelper";
+//import { dateToState, stateToDate } from "../features/ConversionUtil";
+import { dateToState } from "../features/ConversionUtil";
 
 export function Calendar() {
   const dispatch = useDispatch();
@@ -25,14 +27,198 @@ export function Calendar() {
 
   var dragAmount = null;
 
+  /*
+  function generateRow(startNum) {
+    let arr = [];
+    for (let i = startNum; i < startNum + 7; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+
+  function generate4Rows() {
+    let rows = [];
+    for (let i = 0; i < 4; i++) {
+      rows.push(generateRow(i * 7 + 1));
+    }
+    return rows;
+  }
+  let rows = generate4Rows();
+  let headers = ["S", "M", "T", "W", "T", "F", "S"];
+
+  function dayClick(day) {
+    return () => {
+      console.log("day: " + day);
+    };
+  }
+  */
+
   const result = (
     <div>
+      {/* February
+      <table>
+        <tr>
+          {headers.map((header, index) => {
+            return <th key={index}>{header}</th>;
+          })}
+        </tr>
+        {rows.map((row, index1) => {
+          return (
+            <tr key={index1}>
+              {row.map((day, index) => {
+                return (
+                  <td key={index} onClick={dayClick(day)}>
+                    {day}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </table>
+      hello world 1 */}
       <table
         id="calendar"
         style={{ borderCollapse: "collapse", tableLayout: "fixed" }}
         ref={useD3(
           (table) => {
-            drawCalendar({
+            const month = [
+              [1, 2, { d: 3, a: 100 }, 4, 5, 6, 7],
+              [8, 9, 10, 11, 12, 13, 14],
+              [15, 16, 17, 18, 19, 20, 21],
+              [22, 23, 24, 25, 26, 27, 28],
+              [29, 30, 31, -1, -2, -3, -4],
+            ];
+            const monthNames = [
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December",
+            ];
+            const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            const monthNum = 9;
+
+            const header = table.append("thead");
+            const body = table.append("tbody");
+
+            header
+              .append("tr")
+              .append("td")
+              .attr("colspan", 7)
+              .append("h2")
+              .text(monthNames[monthNum])
+              .style("text-align", "center")
+              .style("font-size", "40px");
+
+            header
+              .append("tr")
+              .selectAll("td")
+              .data(dayNames)
+              .enter()
+              .append("td")
+              .text(function (d) {
+                return d;
+              })
+              .style("text-align", "center")
+              .style("font-size", "20px");
+
+            let boxLength = "100px";
+            month.forEach(function (week) {
+              body
+                .append("tr")
+                .selectAll("td")
+                .data(week)
+                .enter()
+                .append("td")
+                .attr("class", function (d) {
+                  return d > 0 ? "" : "empty";
+                })
+                .style("border-style", "solid")
+                .style("border-width", "3px")
+                .style("border-color", "rgb(0,0,0)")
+                .style("width", boxLength)
+                .style("height", boxLength)
+                .on("click", (d) => {
+                  console.log(d.target.__data__);
+                  // TODO add selection mechanism
+                })
+                .each(function (d) {
+                  const td = select(this);
+                  console.log(d);
+                  if (isNaN(d)) {
+                    td.append("div")
+                      .text(function (d) {
+                        return d.d;
+                      })
+                      .style("width", boxLength)
+                      .style("height", "10px")
+                      .style("top", "-33px")
+                      .style("position", "relative");
+                    td.append("div")
+                      .text(function (d) {
+                        return "$" + d.a;
+                      })
+                      .style("width", "95px")
+                      .style("text-align", "center")
+                      .style("top", "-5px")
+                      .style("position", "relative")
+                      .style("font-size", "25px");
+                  } else {
+                    td.append("div")
+                      .text(function (d) {
+                        return d > 0 ? d : "";
+                      })
+                      .style("width", boxLength)
+                      .style("height", boxLength);
+                  }
+                });
+            });
+
+            /*
+            let columns = headers; //["date", "close"];
+            var data = [["2013-08-01"]];
+
+            var thead = table.append("thead");
+            var tbody = table.append("tbody");
+
+            // append the header row
+            thead
+              .append("tr")
+              .selectAll("th")
+              .data(columns)
+              .enter()
+              .append("th")
+              .text(function (column) {
+                return column;
+              });
+
+            // create a row for each object in the data
+            var rows = tbody.selectAll("tr").data(data).enter().append("tr");
+
+            // create a cell in each row for each column
+  */ /*var cells = */ /*rows
+              .selectAll("td")
+              .data(function (row) {
+                return columns.map(function (column) {
+                  return { column: column, value: row[column] };
+                });
+              })
+              .enter()
+              .append("td")
+              .text(function (d) {
+                return d.value;
+              });
+*/
+            return table;
+            /*drawCalendar({
               table: table,
               question: q,
               monthDate: stateToDate(q.dateEarlier),
@@ -48,7 +234,7 @@ export function Calendar() {
               dispatchCallback: (answer) => {
                 dispatch(answer);
               },
-            });
+            });*/
           },
           [q]
         )}
