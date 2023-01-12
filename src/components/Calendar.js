2d7a304733418e3bc5629d78678447050d0764ba
Yahel Nachum <yahelnachum@Yahels-MacBook-Pro.local>
2023-01-12 17:10:00 -0500

    bringing in Next button

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,12 +2,12 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Formik, Form } from "formik";
-import { Button } from "react-bootstrap";
 import { DateTime } from "luxon";
 import {
   getCurrentQuestion,
   getStatus,
   setQuestionShownTimestamp,
+  nextQuestion,
   answer,
 } from "../features/questionSlice";
 import { useD3 } from "../hooks/useD3";
@@ -18,6 +18,9 @@ import { InteractionType } from "../features/InteractionType";
 //import { drawCalendar } from "./CalendarHelper";
 //import { dateToState, stateToDate } from "../features/ConversionUtil";
 import { dateToState } from "../features/ConversionUtil";
+import Grid from "@mui/material/Unstable_Grid2";
+import { styles } from "./ScreenHelper";
+import { Button, Box } from "@mui/material";
 
 export function Calendar() {
   const dispatch = useDispatch();
@@ -56,7 +59,8 @@ export function Calendar() {
 
   const result = (
     <div>
-      {/* February
+      <Grid container style={styles.root} justifyContent="center">
+        {/* February
       <table>
         <tr>
           {headers.map((header, index) => {
@@ -78,160 +82,169 @@ export function Calendar() {
         })}
       </table>
       hello world 1 */}
-      <table
-        id="calendar"
-        style={{ borderCollapse: "collapse", tableLayout: "fixed" }}
-        ref={useD3(
-          (table) => {
-            const month = [
-              [1, 2, { d: 3, a: 100, k: "earlierAmount" }, 4, 5, 6, 7],
-              [8, 9, 10, 11, 12, 13, 14],
-              [15, 16, { d: 17, a: 200 }, 18, 19, 20, 21],
-              [22, 23, 24, 25, 26, 27, 28],
-              [29, 30, 31, -1, -2, -3, -4],
-            ];
-            const monthNames = [
-              "January",
-              "February",
-              "March",
-              "April",
-              "May",
-              "June",
-              "July",
-              "August",
-              "September",
-              "October",
-              "November",
-              "December",
-            ];
-            const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-            const monthNum = 9;
+        <table
+          id="calendar"
+          style={{ borderCollapse: "collapse", tableLayout: "fixed" }}
+          ref={useD3(
+            (table) => {
+              const month = [
+                [1, 2, { d: 3, a: 100, k: "earlierAmount" }, 4, 5, 6, 7],
+                [8, 9, 10, 11, 12, 13, 14],
+                [15, 16, { d: 17, a: 200 }, 18, 19, 20, 21],
+                [22, 23, 24, 25, 26, 27, 28],
+                [29, 30, 31, -1, -2, -3, -4],
+              ];
+              const monthNames = [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+              ];
+              const dayNames = [
+                "Sun",
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thu",
+                "Fri",
+                "Sat",
+              ];
+              const monthNum = 9;
 
-            const header = table.append("thead");
-            const body = table.append("tbody");
+              const header = table.append("thead");
+              const body = table.append("tbody");
 
-            header
-              .append("tr")
-              .append("td")
-              .attr("colspan", 7)
-              .append("h2")
-              .text(monthNames[monthNum])
-              .style("text-align", "center")
-              .style("font-size", "40px");
+              header
+                .append("tr")
+                .append("td")
+                .attr("colspan", 7)
+                .append("h2")
+                .text(monthNames[monthNum])
+                .style("text-align", "center")
+                .style("font-size", "40px");
 
-            header
-              .append("tr")
-              .selectAll("td")
-              .data(dayNames)
-              .enter()
-              .append("td")
-              .text(function (d) {
-                return d;
-              })
-              .style("text-align", "center")
-              .style("font-size", "20px");
-
-            let boxLength = "100px";
-            month.forEach(function (week) {
-              body
+              header
                 .append("tr")
                 .selectAll("td")
-                .data(week)
+                .data(dayNames)
                 .enter()
                 .append("td")
-                .attr("class", function (d) {
-                  return d > 0 ? "" : "empty";
+                .text(function (d) {
+                  return d;
                 })
-                .style("border-style", "solid")
-                .style("border-width", "3px")
-                .style("border-color", "rgb(0,0,0)")
-                .style("width", boxLength)
-                .style("height", boxLength)
-                .on("click", (d) => {
-                  console.log(
-                    "click: target: " + JSON.stringify(d.target.__data__)
-                  );
-                  if (isNaN(d.target.__data__)) {
-                    console.log("click: setselection");
-                    if (d.target.__data__.k !== selection.k) {
-                      if (selection.k === "earlierAmount") {
-                        body
-                          .selectAll("#earlierAmount")
-                          .style("background-color", "steelblue");
-                      } else {
-                        body
-                          .selectAll("#laterAmount")
-                          .style("background-color", "steelblue");
-                      }
-                    }
-                    selection = d.target.__data__;
-                    console.log(
-                      "click: selection: " + JSON.stringify(selection)
-                    );
-                    select(this).style("background-color", "lightblue");
-                  }
-                  // TODO add selection mechanism
-                })
-                .on("mouseover", function (d) {
-                  console.log(
-                    "mouseover: target: " + JSON.stringify(d.target.__data__)
-                  );
-                  if (isNaN(d.target.__data__)) {
-                    select(this).style("background-color", "lightblue");
-                  }
-                })
-                .on("mouseout", function (d) {
-                  console.log(
-                    "mouseout: target: " + JSON.stringify(d.target.__data__)
-                  );
-                  console.log("mouseout: selection: " + selection.d);
-                  if (
-                    isNaN(d.target.__data__) &&
-                    d.target.__data__.d != selection.d
-                  ) {
-                    select(this).style("background-color", "steelblue");
-                  }
-                })
-                .each(function (d) {
-                  const td = select(this);
-                  console.log(d);
-                  if (isNaN(d)) {
-                    td.append("div")
-                      .text(function (d) {
-                        return d.d;
-                      })
-                      .style("width", boxLength)
-                      .style("height", "10px")
-                      .style("top", "-33px")
-                      .style("position", "relative")
-                      .on("click", () => {})
-                      .on("mouseover", function () {})
-                      .on("mouseout", function () {});
-                    td.append("div")
-                      .text(function (d) {
-                        return "$" + d.a;
-                      })
-                      .style("width", "95px")
-                      .style("text-align", "center")
-                      .style("top", "-5px")
-                      .style("position", "relative")
-                      .style("font-size", "25px");
-                    if (d.k === "earlierAmount") td.attr("id", "earlierAmount");
-                    else td.attr("id", "laterAmount");
-                  } else {
-                    td.append("div")
-                      .text(function (d) {
-                        if (!d) return "";
-                        if (d < 1) return "";
-                        return d;
-                      })
-                      .style("width", boxLength)
-                      .style("height", boxLength);
-                  }
-                });
-            });
+                .style("text-align", "center")
+                .style("font-size", "20px");
 
-            /*
+              let boxLength = "100px";
+              month.forEach(function (week) {
+                body
+                  .append("tr")
+                  .selectAll("td")
+                  .data(week)
+                  .enter()
+                  .append("td")
+                  .attr("class", function (d) {
+                    return d > 0 ? "" : "empty";
+                  })
+                  .style("border-style", "solid")
+                  .style("border-width", "3px")
+                  .style("border-color", "rgb(0,0,0)")
+                  .style("width", boxLength)
+                  .style("height", boxLength)
+                  .on("click", (d) => {
+                    console.log(
+                      "click: target: " + JSON.stringify(d.target.__data__)
+                    );
+                    if (isNaN(d.target.__data__)) {
+                      console.log("click: setselection");
+                      if (d.target.__data__.k !== selection.k) {
+                        if (selection.k === "earlierAmount") {
+                          body
+                            .selectAll("#earlierAmount")
+                            .style("background-color", "steelblue");
+                        } else {
+                          body
+                            .selectAll("#laterAmount")
+                            .style("background-color", "steelblue");
+                        }
+                      }
+                      selection = d.target.__data__;
+                      console.log(
+                        "click: selection: " + JSON.stringify(selection)
+                      );
+                      select(this).style("background-color", "lightblue");
+                    }
+                    // TODO add selection mechanism
+                  })
+                  .on("mouseover", function (d) {
+                    console.log(
+                      "mouseover: target: " + JSON.stringify(d.target.__data__)
+                    );
+                    if (isNaN(d.target.__data__)) {
+                      select(this).style("background-color", "lightblue");
+                    }
+                  })
+                  .on("mouseout", function (d) {
+                    console.log(
+                      "mouseout: target: " + JSON.stringify(d.target.__data__)
+                    );
+                    console.log("mouseout: selection: " + selection.d);
+                    if (
+                      isNaN(d.target.__data__) &&
+                      d.target.__data__.d != selection.d
+                    ) {
+                      select(this).style("background-color", "steelblue");
+                    }
+                  })
+                  .each(function (d) {
+                    const td = select(this);
+                    console.log(d);
+                    if (isNaN(d)) {
+                      td.append("div")
+                        .text(function (d) {
+                          return d.d;
+                        })
+                        .style("width", boxLength)
+                        .style("height", "10px")
+                        .style("top", "-33px")
+                        .style("position", "relative")
+                        .on("click", () => {})
+                        .on("mouseover", function () {})
+                        .on("mouseout", function () {});
+                      td.append("div")
+                        .text(function (d) {
+                          return "$" + d.a;
+                        })
+                        .style("width", "95px")
+                        .style("text-align", "center")
+                        .style("top", "-5px")
+                        .style("position", "relative")
+                        .style("font-size", "25px");
+                      if (d.k === "earlierAmount")
+                        td.attr("id", "earlierAmount");
+                      else td.attr("id", "laterAmount");
+                    } else {
+                      td.append("div")
+                        .text(function (d) {
+                          if (!d) return "";
+                          if (d < 1) return "";
+                          return d;
+                        })
+                        .style("width", boxLength)
+                        .style("height", boxLength);
+                    }
+                  });
+              });
+
+              /*
             let columns = headers; //["date", "close"];
             var data = [["2013-08-01"]];
 
@@ -266,8 +279,8 @@ export function Calendar() {
                 return d.value;
               });
 */
-            return table;
-            /*drawCalendar({
+              return table;
+              /*drawCalendar({
               table: table,
               question: q,
               monthDate: stateToDate(q.dateEarlier),
@@ -284,10 +297,41 @@ export function Calendar() {
                 dispatch(answer);
               },
             });*/
-          },
-          [q]
-        )}
-      ></table>
+            },
+            [q]
+          )}
+        ></table>
+      </Grid>
+      <Grid item xs={12} style={{ margin: 0 }}>
+        <Box display="flex" justifyContent="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            disableRipple
+            disableFocusRipple
+            style={styles.button}
+            onClick={() => {
+              /*
+              if (
+                choice !== AmountType.earlierAmount &&
+                choice !== AmountType.laterAmount
+              ) {
+                setError(true);
+                setHelperText("Please choose one of the options below.");
+              } else {
+                setError(false);
+                setHelperText("");
+                dispatch(nextQuestion());
+              }*/
+              dispatch(nextQuestion());
+            }}
+            // disabled={disableSubmit}
+          >
+            {" "}
+            Next{" "}
+          </Button>
+        </Box>
+      </Grid>
       {q.interaction === InteractionType.drag ? (
         <Formik
           initialValues={{ choice: AmountType.none }}
