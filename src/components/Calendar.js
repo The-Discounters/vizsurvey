fe6b9adb09fd1083bd5ce5f6f7b7de91faea486e
Yahel Nachum <yahelnachum@Yahels-MacBook-Pro.local>
2023-01-25 11:04:55 -0500

    remove comments that aren't needed anymore

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -16,7 +16,6 @@ import { useD3 } from "../hooks/useD3";
 import { AmountType } from "../features/AmountType";
 import { InteractionType } from "../features/InteractionType";
 import { drawCalendar } from "./CalendarHelper";
-//import { dateToState, stateToDate } from "../features/ConversionUtil";
 import { dateToState } from "../features/ConversionUtil";
 import Grid from "@mui/material/Unstable_Grid2";
 import { styles } from "./ScreenHelper";
@@ -33,31 +32,6 @@ export function Calendar() {
 
   var dragAmount = null;
 
-  /*
-  function generateRow(startNum) {
-    let arr = [];
-    for (let i = startNum; i < startNum + 7; i++) {
-      arr.push(i);
-    }
-    return arr;
-  }
-
-  function generate4Rows() {
-    let rows = [];
-    for (let i = 0; i < 4; i++) {
-      rows.push(generateRow(i * 7 + 1));
-    }
-    return rows;
-  }
-  let rows = generate4Rows();
-  let headers = ["S", "M", "T", "W", "T", "F", "S"];
-
-  function dayClick(day) {
-    return () => {
-      console.log("day: " + day);
-    };
-  }
-  */
   const [disableSubmit, setDisableSubmit] = React.useState(true);
 
   useEffect(() => {
@@ -94,28 +68,6 @@ export function Calendar() {
   const result = (
     <div>
       <Grid container style={styles.root} justifyContent="center">
-        {/* February
-      <table>
-        <tr>
-          {headers.map((header, index) => {
-            return <th key={index}>{header}</th>;
-          })}
-        </tr>
-        {rows.map((row, index1) => {
-          return (
-            <tr key={index1}>
-              {row.map((day, index) => {
-                return (
-                  <td key={index} onClick={dayClick(day)}>
-                    {day}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </table>
-      hello world 1 */}
         <table
           id="calendar"
           style={{ borderCollapse: "collapse", tableLayout: "fixed" }}
@@ -123,29 +75,8 @@ export function Calendar() {
             (table) => {
               drawCalendar({
                 table: table,
-                //question: q,
-                /*
-                setDisableSubmit: setDisableSubmit,
-                maxTime: q.maxTime,
-                maxAmount: q.maxAmount,
-                interaction: q.interaction,
-                variableAmount: q.variableAmount,
-                amountEarlier: q.amountEarlier,
-                timeEarlier: q.timeEarlier,
-                amountLater: q.amountLater,
-                timeLater: q.timeLater,
-*/
                 onClickCallback: onClickCallback,
                 choice: choice,
-                /*
-                horizontalPixels: q.horizontalPixels,
-                verticalPixels: q.verticalPixels,
-                leftMarginWidthIn: q.leftMarginWidthIn,
-                graphWidthIn: q.graphWidthIn,
-                bottomMarginHeightIn: q.bottomMarginHeightIn,
-                graphHeightIn: q.graphHeightIn,
-                showMinorTicks: q.showMinorTicks,
-*/
                 qDateEarlier: q.dateEarlier,
                 qDateLater: q.dateLater,
                 qAmountEarlier: q.amountEarlier,
