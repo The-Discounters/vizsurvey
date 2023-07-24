03030059b6a75a110b88afe617de0a5677fb20e6
Peter Cordone <pcordone@gmail.com>
2023-07-24 17:53:41 -0400

    Added $PSScript route to script path.

--- scripts/import_exp_config.ps1
+++ scripts/import_exp_config.ps1
@@ -2,22 +2,22 @@ $ErrorActionPreference = "Stop"
 # import the experiment configuration documents
 Write-Output "Start import of configurations."
 Write-Output "Importing experiments"
-dsc -q import -s ./fire_exp_prod.csv -c experiments
+dsc -q import -s $PSScriptRoot/fire_exp_prod.csv -c experiments
 if (!($?)) {
     Throw "dsc failed! See above for error."
 }
 Write-Output "Importing questions"
-dsc -q import -s ./fire_ques_prod.csv -c questions
+dsc -q import -s $PSScriptRoot/fire_ques_prod.csv -c questions
 if (!($?)) {
     Throw "dsc failed! See above for error."
 }
 Write-Output "Importing treatments"
-dsc -q import -s ./fire_treat_prod.csv -c treatments
+dsc -q import -s $PSScriptRoot/fire_treat_prod.csv -c treatments
 if (!($?)) {
     Throw "dsc failed! See above for error."
 }
 Write-Output "Importing treatmentQuestions"
-dsc -q import -s ./fire_treat_ques_prod.csv -c treatmentQuestions
+dsc -q import -s $PSScriptRoot/fire_treat_ques_prod.csv -c treatmentQuestions
 if (!($?)) {
     Throw "dsc failed! See above for error."
 }
