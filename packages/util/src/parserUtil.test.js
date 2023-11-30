a5241048dd6c48dcfa30f513f89c622c60baf070
Peter Cordone <pcordone@gmail.com>
2023-11-30 14:56:03 -0500

    util package test working.

--- packages/util/src/parserUtil.test.js
+++ packages/util/src/parserUtil.test.js
@@ -1,15 +1,23 @@
 import fs from "fs";
-import { parseCSV } from "./parserUtil.js";
+import { parseCSV, parseJSON } from "./parserUtil.js";
 
-describe("parserUtil test.", () => {
+describe("parseCSV test.", () => {
   test("parseCSV test.", async () => {
     const CSV = fs.readFileSync(
-      "/Users/pete/vizsurvey/src/features/test.csv",
+      "/Users/pete/vizsurvey/packages/util/src/test.csv",
       "utf8"
     );
     const result = parseCSV(CSV);
     const strResult = JSON.stringify(result);
     const expctedResult = '[{"column1":"colValue1","column2":"colValue2"}]';
     expect(strResult).toBe(expctedResult);
+  });
+
+  test("parseJSON array input.", async () => {
+    const result = parseJSON(
+      "[[1, 2, 3], [1, 3, 2], [3, 1, 2], [3, 2, 1], [2, 3, 1], [2, 1, 3]]"
+    );
+    expect(result.length).toBe(6);
+    expect(result[0].length).toBe(3);
   });
 });
